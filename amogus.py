0aba7a03bbb81977a660b874837de2292e2a0216
Kreato <gamzeli_kerim@outlook.com>
2021-03-22 14:47:53 +0300

    Create amogus.py

---
+++ amogus.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import discord
+
+class amogus(discord.Client):
+    async def on_ready(self):
+        print('Imposter:')
+        print(self.user.name)
+        print(self.user.id)
+        print('-------------')
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('!amogus'):
+            await message.reply('When the imposter is sus!', mention_author=True)
+
+client = amogus()
+client.run('token')
